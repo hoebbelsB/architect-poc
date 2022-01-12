76dbfc3f619f51b7a2919b149e50f61c2f83ed4c
ondrejsvoren <ondrej.svoren@flowup.cz>
2022-01-12 16:08:47 +0100

    refactor(settings-modal): minor changes

--- libs/settings/public/feature-settings-modal/src/lib/feature-settings-modal.adapter.ts
+++ libs/settings/public/feature-settings-modal/src/lib/feature-settings-modal.adapter.ts
@@ -4,16 +4,16 @@ import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class FeatureSettingsModalAdapter {
-  actionHistory$ = this.localState.settingsState$.pipe(
+  readonly actionHistory$ = this.settingsStore.settingsState$.pipe(
     map(({ history }) => history)
   );
-  activeMenu$ = this.localState.activeMenu$;
-  menuItems$ = this.localState.menuItems$;
+  readonly activeMenu$ = this.settingsStore.activeMenu$;
+  readonly menuItems$ = this.settingsStore.menuItems$;
+  readonly showMenu$ = this.settingsStore.showMenu$;
 
-showMenu$ = this.localState.showMenu$;
-  constructor(private readonly localState: SettingsStore) {}
+  constructor(private readonly settingsStore: SettingsStore) {}
 
   triggerAction(action: string): void {
-    this.localState.writeAction({type: action});
+    this.settingsStore.writeAction({type: action});
   }
 }
